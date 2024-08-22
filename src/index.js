5ba6164a51972bea3d6b5e38268f89bce27445a8
Mark Neumann <mark.neumann.sm@gmail.com>
2024-08-22 11:29:19 +0300

    feat: create task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = Math.abs(n).toString();
+    let reversedStr = str.split("").reverse().join("");
+    return parseInt(reversedStr);
+};
